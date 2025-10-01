eae764e59d375d348ada33a49e1135230d009958
rosana <rosana@opseeker.com>
2025-10-01 18:20:30 +0200

    feat(start): Blog inicial con Astro

---
+++ my-blog/astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://Rockyvar827.github.io/vivirsinprivilegios', // ⚡️ cámbialo
+  base: '/vivirsinprivilegios/', // si no es repo de usuario, agrega esta línea
+});
